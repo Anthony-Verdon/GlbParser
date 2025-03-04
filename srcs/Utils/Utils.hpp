bcc0e3b2dbe0f2bffc9ce23af38b7dbf5a69e226
averdon <anthonyverdon10@gmail.com>
2025-03-04 17:18:43 +0100

    suppress Utils.cpp, declare function in .hpp to add static keyword and keep them in the lib. Also suppress useless Utils functions

--- srcs/Utils/Utils.hpp
+++ srcs/Utils/Utils.hpp
@@ -1,16 +1,35 @@
 #pragma once
 
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
 
 namespace Utils
 {
-  bool isInt(const std::string &word);
-  bool isFloat(const std::string &words);
-  std::vector<std::string> splitLine(std::string line, const std::string &delimiter);
-  float DegToRad(float angle);
-  std::string readFile(const std::string &path, std::ios_base::openmode mode = std::ios_base::in);
-  bool checkExtension(const std::string &path, const std::string &extension);
+    static std::string readFile(const std::string &path, std::ios_base::openmode mode = std::ios_base::in )
+    {
+        std::ifstream file;
+        std::stringstream stream;
+
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(path, mode);
+        stream << file.rdbuf();
+        file.close();
+        return (stream.str());
+    }
+
+    static bool checkExtension(const std::string &path, const std::string &extension)
+    {
+        std::string filename = path.substr(path.find_last_of("/") + 1, path.length());
+        if (extension.size() >= filename.size())
+            return (false);
+        for (size_t i = 0; i < extension.size(); i++)
+        {
+            if (filename[filename.size() - extension.size() + i] != extension[i])
+                return (false);
+        }
+        return (true);
+    }
 }
   
